36fb095a63d079962f7c18a23f9efa093f611804
bautisanpedro <90062367+bautisanpedro@users.noreply.github.com>
2022-11-17 16:06:21 -0300

    navegacion

--- src/navegation/TabNavigation.js
+++ src/navegation/TabNavigation.js
@@ -1,7 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { Ionicons, Entypo, FontAwesome } from '@expo/vector-icons'
-
+import Profile from '../screens/MyProfile/MyProfile'
 import HomeNavigation from './HomeNavigation'
 
 const Tab = createBottomTabNavigator()
@@ -17,6 +17,14 @@ export default function TabNavigation() {
           tabBarIcon: () => <Ionicons name='ios-home' color={'#0095F6'} size={35} />,
           headerShown:false
         }}
+        />
+        <Tab.Screen 
+        name='Profile' 
+        component={Profile}
+        options={{
+          tabBarIcon: () => <Ionicons name='ios-person-circle' color={'#0095F6'} size={40} />,
+          headerShown:false
+        }} 
         />
 
       
