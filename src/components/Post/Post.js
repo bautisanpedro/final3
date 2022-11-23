8520618a5a8d0d3bee0993af797f20dd5785effb
GPreuss <gpreuss@udesa.edu.ar>
2022-11-23 13:03:54 -0300

    andiamo

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -28,6 +28,7 @@ class Post extends Component {
                 MyLike:true
             })
         }
+        console.log(this.props.data)
     }   
 
     like(){
@@ -74,26 +75,29 @@ class Post extends Component {
             
             <View style={styles.container}> 
                 <Image 
-                    style={styles.image}
-                    source={{uri: `${this.props.data.foto}`}}
-                />
+                        style={styles.image}
+                        source={{uri: this.props.data.foto}}
+                        resizeMode='cover'
+                    />
                 <View>
                     <Text style={styles.subtitle}>Descripcion:</Text>
                     <Text>{this.props.data.descripcion}</Text>
                 </View>
-            <View>
-            <Text>{this.state.likesCount}</Text>
-            {
-                this.state.MyLike ?
-                    <TouchableOpacity onPress={()=> this.unlike()}>
-                        <FontAwesome name='heart' color='red' size={32} />
-                    </TouchableOpacity>
-                :
-                    <TouchableOpacity onPress={()=> this.like()}>
-                        <FontAwesome name='heart-o' color='red' size={32} />
-                    </TouchableOpacity>
-            }
-            </View>
+                
+                <View>
+                    
+                    <Text>{this.state.likesCount}</Text>
+                    {
+                        this.state.MyLike ?
+                        <TouchableOpacity onPress={()=> this.unlike()}>
+                            <FontAwesome name='heart' color='red' size={32} />
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPress={()=> this.like()}>
+                            <FontAwesome name='heart-o' color='red' size={32} />
+                        </TouchableOpacity>
+                    }
+                </View>
                 <TouchableOpacity onPress={()=> this.props.navigation.navigate('Comments',{id: this.props.id})}>
                     <Text>Agregar comentario</Text>
                 </TouchableOpacity>
@@ -104,18 +108,26 @@ class Post extends Component {
 
 const styles = StyleSheet.create({
     container:{
-        flexDirection: 'row',
-        paddingHorizontal:10,
-        paddingVertical:16,
+        flexDirection: 'column',
+        paddingHorizontal:0,
+        paddingVertical:0,
         justifyContent:'space-between',
-        marginVertical:16,
+        marginVertical:6,
         marginHorizontal:10,
         borderWidth:.5,
         borderRadius:10
     },
     subtitle:{
         fontWeight:700,
-    }
+    },
+    image:{
+        marginTop: 10,
+        marginBottom: 8,
+        height:300,
+        width: 300,
+        borderRadius: 8,
+        alignItems: 'center',
+    },
 })
 
 export default Post
